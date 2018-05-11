b63026a0756639012fa6246f67eafb7c3446be52
Kaesaru <jkit@JKits-MBP.home>
2018-05-10 21:51:51 -0400

    intial RWC website

---
+++ src/Websites.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+
+const Websites = () => (
+  <div className="websites">
+    <div className="career">
+      <h2 className="career"></h2>
+    </div>
+    <div className="onlineEducation"></div>
+    <div className="books"></div>
+  </div>
+); 
+
+
+export default Websites;
