38607a3f35d67b501409b73e83dc3cead1e38b37
Kaesaru <fitzj1@umbc.edu>
2018-05-31 11:51:32 -0400

    updated TODO for Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 const Header = () => (
   // TODO add styling with themes if desired {classes.header}
   <div className='header'>
+    {/* TODO fix this img issue, find correct path */}
     <img src='http:localhost:3000/Users/jkit/FullStack_local/websites/careerDay/src/logo.svg' alt='logo' 
     />
     <h1>Career Path</h1>  
