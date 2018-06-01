ed7b8d05d6de5bca2a24d9429417669fec302947
Kaesaru <fitzj1@umbc.edu>
2018-06-01 00:08:59 -0400

    updated Disocver positioning

--- src/Components/Main/Discover.js
+++ src/Components/Main/Discover.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// const img = '../../images/outdoorchallenge-clouds-cloudscape.jpg'
+
 const Discover = () => (
-  <div>
+  <div className='discover'>
     {/* TODO apply a background image  */}
     {/* TODO design accent shapes */}
 
     {/* TODO place in center of div */}
-    <h2>Discvoer you Path</h2>
-
+    <div>
+      <h2>Discvoer Your Path</h2>
+    </div>
     {/* TODO add career bubbles/shapes with different careers */}
     {/* TODO add link to all the careers from presenters */}
     <button>Discover More</button>
