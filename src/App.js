91cd22d1a1bcde59a92abf065817eeee99f81dc5
Kaesaru <fitzj1@umbc.edu>
2018-07-06 00:01:46 -0400

    deployed redesign and created Assignment.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Discover from './Components/Main/Discover';
 import Carousel from './Components/Main/Carousel';
 import MentorsList from './Components/MentorsComp/MentorsList';
 import QuickTips from './Components/Main/QuickTips';
+import Assignment from './Components/Main/Assignment';
 import Footer from './Components/Main/Footer';
 
 // removed and updated
@@ -26,17 +27,9 @@ class App extends Component {
         <Discover />
         <Carousel />
         <MentorsList />
-
         <QuickTips />
-    
-
-        <div className="assignment">
-        <h3 className='title'>Assignment</h3>
-          <h5>What do you want to be when you grow up and why?</h5>
-          <p className='bodyOne'>Think about what interests you have. Then on a piece paper, your phone, computer, or other device write 2 or 3 careers that would be of interest to you. After picking your career, explain why you picked that career and what you think you need to do in order to be successful at that career. Afterwards take the information you gathered and send it to the email below explaining what you want to be and why.</p> 
-          <p><strong>Email: </strong>Julius@CareerDayRWC.info</p>
-        </div>
-          <Footer />
+        <Assignment />
+        <Footer />
       </div>
     );
   }
---
+++ src/Components/Main/Assignment.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Assignment = () => (
+  <div className="assignment">
+    <h3 className='title'>Assignment</h3>
+    <h5>What do you want to be when you grow up and why?</h5>
+
+    <p className='bodyOne'>Think about what interests you have. Then on a piece paper, your phone, computer, or other device write 2 or 3 careers that would be of interest to you. After picking your career, explain why you picked that career and what you think you need to do in order to be successful at that career. Afterwards take the information you gathered and send it to the email below explaining what you want to be and why.</p> 
+
+    <p><strong>Email: </strong>Julius@CareerDayRWC.info</p>
+  </div>
+);
+
+export default Assignment;
