a0ae01f3e98c9155d820c4b489f1bf470d53d36a
Kaesaru <fitzj1@umbc.edu>
2018-07-06 01:09:47 -0400

    ISSUE trying to change buttons to links for react router Link not definied

--- src/Components/Main/Nav.js
+++ src/Components/Main/Nav.js
@@ -8,15 +8,15 @@ const Nav = () => (
     {/* TODO reorder more relavent to the left */}
     {/* TODO add functionality to the Buttons to go to desired pages */}
     {/* TODO style buttons and orient on website */}
-    <button>About</button>
+    <Link to='about'>About</Link>
     {/* TODO add drop down functionality with Finance, Skills, Travel */}
-    <button>Mentors</button>
-    <button>Tutorial</button>
+    <Link to='mentors'>Mentors</Link>
+    <Link to='tutorial'>Tutorial</Link>
     {/* TODO add drop down funcitonality with Free courses, College */}
-    <button>Education</button>
-    <button>Life</button>
-    <button>Resources</button>
-    <button>Contact</button>    
+    <Link to='dducation'>Education</Link>
+    <Link to='life'>Life</Link>
+    <Link to='resources'>Resources</Link>
+    <Link to='contact'>Contact</Link>    
   </div>
 );
 
